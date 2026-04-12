b2652ba7ba31c6e66746f01a102412d8e460355c
ChYMean <13660751269@163.com>
2026-04-12 17:02:36 +0800

    test EXP1

---
+++ EXP1/maze_pro.cpp
@@ -0,0 +1,7 @@
+#include "maze_pro.h"
+
+int main()
+{
+    cout << "Delay no more" << endl;
+    return 0;
+}
---
+++ EXP1/maze_pro.h
@@ -0,0 +1,7 @@
+#ifndef __MAZE_PRO_H__
+#define __MAZE_PRO_H__
+#include <iostream>
+#include <stack>
+using namespace std;
+
+#endif /* end maze_pro */
