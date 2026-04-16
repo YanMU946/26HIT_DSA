14bdc3584b7b0141faac46f14eed519eaea07772
ChYMean <13660751269@163.com>
2026-04-16 12:05:30 +0800

    partly finish EXP1

--- EXP1/maze_pro.cpp
+++ EXP1/maze_pro.cpp
@@ -2,6 +2,37 @@
 
 int main()
 {
-    cout << "Delay no more" << endl;
+    Maze<14, 12> muggle({
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, },
+        { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, },
+        { 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, },
+        { 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, },
+        { 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, },
+        { 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, },
+        { 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, },
+        { 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, },
+        { 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, },
+        { 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, },
+        { 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, },
+        { 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, },
+        });
+    muggle.print();
+
+    if (muggle.SolvePuzzle())
+    {
+        std::cout << "Planned successfully" << std::endl;
+        muggle.print();
+    }
+    else
+        std::cout << "Failed" << std::endl;
+        
+    if (muggle.SolvePuzzle_Recursive())
+    {
+        std::cout << "Planned successfully" << std::endl;
+        muggle.print();
+    }
+    else
+        std::cout << "Failed" << std::endl;
     return 0;
 }
+
--- EXP1/maze_pro.h
+++ EXP1/maze_pro.h
@@ -1,7 +1,196 @@
 #ifndef __MAZE_PRO_H__
 #define __MAZE_PRO_H__
 #include <iostream>
+#include <vector>
 #include <stack>
-using namespace std;
+
+struct Point
+{
+    int x;
+    int y;
+
+    Point(int _x, int _y) :x(_x), y(_y) {}
+
+    bool operator==(Point _point) { return (x == _point.x && y == _point.y); }
+};
+
+template<int _col, int _row>
+class Maze
+{
+public:
+    Maze() {}
+
+    Maze<_col, _row>(const std::vector<std::vector<int>>& _maze, const Point& _start = Point(0, 0), const Point& _end = Point(_row - 1, _col - 1))
+        :start(_start), end(_end), row(_row), col(_col)
+    {
+        maze = _maze;
+        maze[start.x][start.y] = maze[end.x][end.y] = 0;
+    }
+
+    void print()
+    {
+        for (int i = 0;i < col + 2;i++)
+        {
+            if (i >= 1 && i <= col)
+                std::cout << "* ";
+            else
+                std::cout << "# ";
+        }
+        std::cout << std::endl;
+
+        for (int i = 0;i < row;i++)
+        {
+            std::cout << "* ";
+            for (int j = 0;j < col;j++)
+            {
+                if (Point(i, j) == start)
+                {
+                    std::cout << "S ";
+                    continue;
+                }
+                else if (Point(i, j) == end)
+                {
+                    std::cout << "E ";
+                    continue;
+                }
+                else if (maze[i][j] == 0)
+                {
+                    std::cout << "  ";
+                    continue;
+                }
+                else if (maze[i][j] == -1)
+                {
+                    std::cout << "X ";
+                    maze[i][j] = 0;
+                    continue;
+                }
+                else
+                {
+                    std::cout << "¨€ ";
+                    continue;
+                }
+            }
+            std::cout << "* " << std::endl;
+        }
+        for (int i = 0;i < col + 2;i++)
+        {
+            if (i >= 1 && i <= col)
+                std::cout << "* ";
+            else
+                std::cout << "# ";
+        }
+        std::cout << std::endl;
+    }
+
+    bool IsValid(const Point& _point)
+    {
+        return (_point.x >= 0 && _point.x < row && _point.y >= 0 && _point.y < col && maze[_point.x][_point.y] == 0);
+    }
+
+    bool SolvePuzzle()
+    {
+        std::stack<Point> maze_point;
+        maze_point.push(start);
+
+        std::vector<std::vector<bool>> IsVisited(row, std::vector<bool>(col, false));
+        IsVisited[start.x][start.y] = true;
+
+        int move_x[] = { -1, 0, 1,-1, 1,-1, 0, 1 };
+        int move_y[] = { -1,-1,-1, 0, 0, 1, 1, 1 };
+
+        while (!maze_point.empty())
+        {
+            Point cur = maze_point.top();
+
+            if (cur == end)
+            {
+                while (!maze_point.empty())
+                {
+                    cur = maze_point.top();
+                    maze[cur.x][cur.y] = -1;
+                    maze_point.pop();
+                }
+                return true;
+            }
+
+            bool IsSuccess = false;
+
+            for (int i = 0;i < 8;i++)
+            {
+                Point n_cur(cur.x + move_x[i], cur.y + move_y[i]);
+                if (IsValid(n_cur) && !IsVisited[n_cur.x][n_cur.y])
+                {
+                    maze_point.push(n_cur);
+                    IsVisited[n_cur.x][n_cur.y] = true;
+                    IsSuccess = true;
+                    break;
+                }
+            }
+
+            if (!IsSuccess)
+            {
+                maze_point.pop();
+            }
+        }
+
+        return false;
+    }
+
+    bool SolvePuzzle_Recursive()
+    {
+        std::stack<Point> maze_point;
+        maze_point.push(start);
+
+        std::vector<std::vector<bool>> IsVisited(row, std::vector<bool>(col, false));
+        IsVisited[start.x][start.y] = true;
+
+        int move_x[] = { -1, 0, 1,-1, 1,-1, 0, 1 };
+        int move_y[] = { -1,-1,-1, 0, 0, 1, 1, 1 };
+
+        while (!maze_point.empty())
+        {
+            Point cur = maze_point.top();
+
+            if (cur == end)
+            {
+                while (!maze_point.empty())
+                {
+                    cur = maze_point.top();
+                    maze[cur.x][cur.y] = -1;
+                    maze_point.pop();
+                }
+                return true;
+            }
+
+            bool IsSuccess = false;
+
+            for (int i = 0;i < 8;i++)
+            {
+                Point n_cur(cur.x + move_x[i], cur.y + move_y[i]);
+                if (IsValid(n_cur) && !IsVisited[n_cur.x][n_cur.y])
+                {
+                    maze_point.push(n_cur);
+                    IsVisited[n_cur.x][n_cur.y] = true;
+                    IsSuccess = true;
+                    break;
+                }
+            }
+
+            if (!IsSuccess)
+            {
+                maze_point.pop();
+            }
+        }
+
+        return false;
+    }
+
+private:
+    std::vector<std::vector<int>> maze;
+    int row;
+    int col;
+    Point start;
+    Point end;
+};
 
 #endif /* end maze_pro */
